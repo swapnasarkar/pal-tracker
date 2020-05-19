ada253111da8347973b4301214348b3e6d18545e
swapnasarkar <swapnasarkar@gmail.com>
2020-05-19 06:09:49 +0000

    WelcomeController 1.0

---
+++ src/PalTracker/WelcomeController.cs
@@ -0,0 +1,12 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PalTracker
+{
+    [Route("/")]
+    public class WelcomeController : ControllerBase
+    {
+        [HttpGet]
+        public string SayHello() => "hello";
+    }
+}
